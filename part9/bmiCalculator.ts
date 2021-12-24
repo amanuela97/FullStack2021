b30892e7f1b056d920513004f0830ee9062f47a8
amanuela97 <babyzewdie@gmail.com>
2021-12-24 15:36:51 +0200

    9.1 completed

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,25 @@
+
+const calculateBmi = (height: number, weight: number): String => {
+
+    if(isNaN(height)) return 'provide a valid height'
+    if(isNaN(weight)) return 'provide a valid weight'
+
+    const bmi = Number((weight / Math.pow((height / 100),2)).toFixed(1))
+    let result: String = ''
+    if(bmi < 18.5) {
+        result = `Underweight (${height} ${weight})`
+    }else if(bmi >= 18.5 && bmi < 25){
+        result = `Normal (${height} ${weight})`
+    }else if(bmi >= 25 && bmi < 30){
+        result = `Overweight (${height} ${weight})` 
+    }else if(bmi >= 30 && bmi < 35){
+        result = `Obesity class 1 (${height} ${weight})` 
+    }else if(bmi >= 35 && bmi <= 40){
+        result = `Obesity class 2 (${height} ${weight})` 
+    }else {
+        result = `Obesity class 3 (${height} ${weight})` 
+    }
+    return result
+}
+
+console.log(calculateBmi(180, 70))
